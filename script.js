fb833a182bbf8f6371d8b04e5429a582c2b7f638
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-12 01:02:13 +0600

    button-effect-done

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener("mouseover", function (event) {
+  const x = event.pageX - btn.offsetLeft;
+  const y = event.pageY - btn.offsetTop;
+  btn.style.setProperty("--xPos", x + "px");
+  btn.style.setProperty("--yPos", y + "px");
+});
